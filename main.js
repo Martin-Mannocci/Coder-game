fbda1d822955d60df86bcedc1634f9fef587d007
Martin Mannocci <martinmannocci@hotmail.com>
2021-11-22 18:44:45 -0300

    primera pre-entrega

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-const lobo = {
-    nombre: "Lobo",
-    hp: 15,
-    atk: 2,
-    def: 1
-};
-
 player = prompt("Ingrese su nombre")
+
+
+
+function Enemy(nombre, atk, hp, def) {
+    this.nombre = nombre;
+    this.atk = atk;
+    this.hp = hp;
+    this.def = def;
+}
+
+
+function Weapons(nombre, minAtk, maxAtk) {
+    this.nombre = nombre;
+    this.minAtk = minAtk;
+    this.maxAtk = maxAtk;
+}
+
+const dagger = new Weapons("Dagas", 1, 7)
+const sword = new Weapons("Espada", 4, 4)
+const fist = new Weapons("Puño", 3, 5)
+
 
 const playerLvl1 = {
     nombre: player,
@@ -13,6 +27,36 @@ const playerLvl1 = {
     atk: 5,
     def: 5
 };
+
+const lobo = {
+    nombre: "Lobo",
+    hp: 15,
+    atk: 2,
+    def: 1
+};
+
+let weaponArray = [
+    dagger, sword
+]
+
+//desafios:
+weaponArray.push(fist)
+
+let sortArray = weaponArray.sort((a, c) => a.nombre.length - c.nombre.length);
+console.log("Array", sortArray)
+
+//fin desafios
+
+let getRandomAtk = function (min, max) {
+    let randomAtk = Math.floor((Math.random() * max) + min);
+    while (randomAtk > max) {
+        randomAtk = Math.floor((Math.random() * max) + min)
+    }
+    return randomAtk;
+}
+
+
+
 
 let arma = prompt(
     "Frente a vos, encontras dos tipos de sets de armas. Un par de dagas, o una espada y un escudo. Tambien podrias elegir ser orgulloso y confiar en que tus puños harian mejor el trabajo \n\n Ingrese 'espada', 'dagas, o 'puños' "
@@ -42,31 +86,48 @@ const StartGame = () => {
         "Un lobo es lo primero que cruza tu camino. Tomas tu " + arma + " y te preparas para enfrentarlo"
     );
 
-    Battle();
+    battle();
 };
+
+
 
 const batalla = (a, b) => a - b;
 
-const Battle = () => {
+const battle = () => {
 
-   if  (lobo.hp != 0 && playerLvl1.hp != 0)
-    while (lobo.hp != 0 && playerLvl1.hp != 0) {
-        alert(
-            "Levantas tu " + arma + " atacando al lobo con toda la fuerza que tenes"
-        );
-        lobo.hp = batalla(lobo.hp, playerLvl1.atk);
-        playerLvl1.hp = batalla(playerLvl1.hp, lobo.atk);
-        console.log("Los puntos de salud del lobo son", lobo.hp);
-        console.log("los puntos de salud de " + player + " son", playerLvl1.hp);
-    }
+    if (lobo.hp != 0 && playerLvl1.hp != 0)
+        while (lobo.hp != 0 && playerLvl1.hp != 0) {
+            alert(
+                "Levantas tu " + arma + " atacando al lobo con toda la fuerza que tenes"
+            );
+            lobo.hp = batalla(lobo.hp, playerLvl1.atk);
+            playerLvl1.hp = batalla(playerLvl1.hp, lobo.atk);
+            console.log("Los puntos de salud del lobo son", lobo.hp);
+            console.log("los puntos de salud de " + player + " son", playerLvl1.hp);
+        }
 
-    if (lobo.hp <= 0 ) {
+    if (lobo.hp <= 0) {
         alert("Pudiste derrotar al lobo con facilidad y continuas tu camino.")
-    }
-
-    else if  (playerLvl1.hp <= 0) {
+    } else if (playerLvl1.hp <= 0) {
         alert("El lobo obtuvo lo mejor de vos en este combate. Fin del juego.")
     }
 }
 
 StartGame();
+
+alert("Finalmente llegas al campamento y despues de tu merecido descanso te pones a ver de lo que es capaz tu arma. Decidis atacar al muñeco de paja.");
+
+switch (arma) {
+    case "espada":
+        console.log("Decidis probar tu espada contra el muñeco de paja y despues de tu ataque de 99 puntos de salud inciales dejas el muñeco de paja en " + batalla(99, getRandomAtk(sword.minAtk, sword.maxAtk)) + " puntos de salud");
+        break;
+
+    case "dagas":
+        console.log("Decidis probar tus dagas contra el muñeco de paja y despues de tu ataque de 99 puntos de salud inciales dejas el muñeco de paja en " + batalla(99, getRandomAtk(dagger.minAtk, dagger.maxAtk)) + " puntos de salud");
+        break
+
+    case "puños":
+        console.log("Decidis probar tus puños contra el muñeco de paja y despues de tu ataque de 99 puntos de salud inciales dejas el muñeco de paja en " + batalla(99, getRandomAtk(fist.minAtk, fist.maxAtk)) + " puntos de salud");
+        break
+
+}
